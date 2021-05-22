8b8eb86c597c7c7a4535303b14b1d422fe0367a5
Arkadiy Asuratov <arkady.asu@outlook.com>
2021-05-22 09:21:36 +0200

    update license header

--- src/files/application/Espo/Modules/DubasUuidField/Entities/UuidManager.php
+++ src/files/application/Espo/Modules/DubasUuidField/Entities/UuidManager.php
@@ -1,22 +1,23 @@
 <?php
 
 /*
- * This file is part of the Dubas UUID Field package.
+ * This file is part of the Dubas UUID Field - EspoCRM extension.
  *
- * Copyright (C) 2021 DUBAS - Emil Dubielecki & Arkadiy Asuratov S.C.
+ * DUBAS S.C. - contact@dubas.pro
+ * Copyright (C) 2021 Arkadiy Asuratov, Emil Dubielecki
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License
- * as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 namespace Espo\Modules\DubasUuidField\Entities;
