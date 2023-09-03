94c259f2d45b3aaaae5e88759d2edfaa3f671f05
Arkadiy Asuaratov <arkadiy.asuratov@dubas.pro>
2023-09-03 10:10:34 +0200

    chore(dev): update rector.php

--- rector.php
+++ rector.php
@@ -1,43 +1,48 @@
 <?php
+/**
+ * This file is part of the Dubas UUID Field - EspoCRM extension.
+ *
+ * DUBAS S.C. - contact@dubas.pro
+ * Copyright (C) 2021-2023 Arkadiy Asuratov, Emil Dubielecki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\SetList;
-use Rector\Set\ValueObject\DowngradeSetList;
-use Rector\Set\ValueObject\DowngradeLevelSetList;
 use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        DowngradeLevelSetList::DOWN_TO_PHP_74,
-        SetList::PHP_74,
-        SetList::TYPE_DECLARATION_STRICT,
-        SetList::CODE_QUALITY,
-        SetList::NAMING,
-    ]);
-
-    $rectorConfig->autoloadPaths([
-        __DIR__ . '/site',
-    ]);
+    $rectorConfig->phpVersion(PhpVersion::PHP_80);
 
     $rectorConfig->paths([
-        __DIR__ . '/ecs.php',
-        __DIR__ . '/rector.php',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__ . '/site/bootstrap.php',
+        __DIR__ . '/src/files/custom',
     ]);
 
     $rectorConfig->skip([
         __DIR__ . '/src/*/vendor/*',
     ]);
 
-    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses();
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_80,
+    ]);
 
-    // register single rule
-    $rectorConfig->rule(TypedPropertyRector::class);
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
 };
